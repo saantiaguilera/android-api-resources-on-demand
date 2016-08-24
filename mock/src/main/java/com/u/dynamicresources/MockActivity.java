1d424b96cc8b476c2e96b24bab70c19c5a65c470
saantiaguilera <santiago.aguilera@mercadolibre.com>
2016-08-24 11:50:31 -0300

    Dependency to lib in mock module

---
+++ mock/src/main/java/com/u/dynamicresources/MockActivity.java
@@ -0,0 +1,17 @@
+package com.u.dynamicresources;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * Created by saguilera on 8/24/16.
+ */
+public class MockActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    
+}
