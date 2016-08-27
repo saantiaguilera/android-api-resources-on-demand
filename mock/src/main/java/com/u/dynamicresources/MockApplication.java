c1d050c167cef9d7364dca014671dd057deb8a0b
saantiaguilera <santiago.aguilera@mercadolibre.com>
2016-08-26 22:52:41 -0300

    Added strict mode

--- mock/src/main/java/com/u/dynamicresources/MockApplication.java
+++ mock/src/main/java/com/u/dynamicresources/MockApplication.java
@@ -1,6 +1,7 @@
 package com.u.dynamicresources;
 
 import android.app.Application;
+import android.os.StrictMode;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.u.dynamic_resources.core.Pomu;
@@ -13,6 +14,15 @@ public class MockApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                .detectAll()// or .detectAll() for all detectable problems
+                .penaltyLog()
+                .build());
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                .detectAll()
+                .penaltyLog()
+                .build());
 
         Fresco.initialize(this);
         Pomu.initialize(this);
