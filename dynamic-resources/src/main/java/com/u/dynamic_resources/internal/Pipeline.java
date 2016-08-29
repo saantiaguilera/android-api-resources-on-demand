ce0e8bf1870023497d34a2534303be10f76d7c7f
saantiaguilera <santiago.aguilera@mercadolibre.com>
2016-08-29 11:30:23 -0300

    Pipeline javadoc

--- dynamic-resources/src/main/java/com/u/dynamic_resources/internal/Pipeline.java
+++ dynamic-resources/src/main/java/com/u/dynamic_resources/internal/Pipeline.java
@@ -8,6 +8,7 @@ import com.u.dynamic_resources.core.Configurations;
 import okhttp3.OkHttpClient;
 
 /**
+ * Pipeline to send requests to a streamer and manage cache/stuff
  * Created by saguilera on 8/25/16.
  */
 public class Pipeline {
@@ -17,6 +18,10 @@ public class Pipeline {
     private @Nullable OkHttpClient client;
     private @Nullable Cache cache;
 
+    /**
+     * Singleton instance for the pipeline
+     * @return Pipeline instance
+     */
     public static @NonNull Pipeline getInstance() {
         if (instance == null) {
             synchronized (Pipeline.class) {
@@ -29,25 +34,44 @@ public class Pipeline {
         return instance;
     }
 
+    /**
+     * Private constructor
+     */
     private Pipeline() {
         client = null;
+        cache = null;
     }
 
+    /**
+     * Set custom configurations to the pipeline
+     *
+     * @param configurations to set to the pipeline
+     */
     public void setConfigurations(@NonNull Configurations configurations) {
         this.client = configurations.getClient();
         this.cache = configurations.getCache();
     }
 
+    /**
+     * Clear all the current data cached on disk
+     */
     public void clearCaches() {
         if (cache != null) {
             cache.clear();
         }
     }
 
+    /**
+     * Fetch a particular request
+     * @param request with the data to fetch
+     */
     @SuppressWarnings("ConstantConditions")
     public void fetch(@NonNull Request request) {
-        Streamer.Builder builder = Streamer.create()
-                .cache(cache);
+        Streamer.Builder builder = Streamer.create();
+
+        if (cache != null) {
+            builder.cache(cache);
+        }
 
         if (client != null) {
             builder.client(client);
