d85267ecf72f47529a54405786feb15bfbd41ae4
saantiaguilera <santiago.aguilera@mercadolibre.com>
2016-08-29 10:48:16 -0300

    Callbacks javadoc

--- dynamic-resources/src/main/java/com/u/dynamic_resources/internal/loading/FileCallback.java
+++ dynamic-resources/src/main/java/com/u/dynamic_resources/internal/loading/FileCallback.java
@@ -5,12 +5,21 @@ import android.support.annotation.NonNull;
 import java.io.File;
 
 /**
+ * Internal class for callbacks of the file request
  * Created by saguilera on 8/25/16.
  */
 public interface FileCallback {
 
-    void onFailure(@NonNull Exception e);
-
+    /**
+     * When the request was performed successfully
+     * @param file where the image download resides
+     */
     void onSuccess(@NonNull File file);
+
+    /**
+     * When the request failed
+     * @param e exception thrown
+     */
+    void onFailure(@NonNull Exception e);
 
 }
