28f871a9f57a1d1dfec765dfca89bfb6fc82e53c
saantiaguilera <santiago.aguilera@mercadolibre.com>
2016-08-29 11:42:28 -0300

    More javadoc

--- dynamic-resources/src/main/java/com/u/dynamic_resources/internal/Streamer.java
+++ dynamic-resources/src/main/java/com/u/dynamic_resources/internal/Streamer.java
@@ -21,6 +21,9 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 /**
+ * Inner class that performs the networking fetches of a request + cache calls if already exists
+ * to avoid useless calls
+ *
  * Created by saguilera on 8/25/16.
  */
 final class Streamer {
@@ -42,10 +45,21 @@ final class Streamer {
     //For writing files
     private static final ExecutorService executor = Executors.newFixedThreadPool(1);
 
+    /**
+     * Creator of a streamer immutable object
+     * @return Builder
+     */
     public static Builder create() {
         return new Builder();
     }
 
+    /**
+     * Private constructor
+     * @param client for networking calls
+     * @param callback for status
+     * @param cache cache used for the images
+     * @param uri to fetch
+     */
     @SuppressWarnings("ConstantConditions")
     private Streamer(@Nullable OkHttpClient client,
                      @Nullable FileCallback callback,
@@ -62,13 +76,19 @@ final class Streamer {
         this.uri = uri;
     }
 
-    @UiThread
+    /**
+     * Fetch the given Uri from network if cache doesnt has it, else retreive it from cache
+     * and callback to user.
+     *
+     * Runs on UI Thread or worker thread
+     */
     private void fetch() {
         if (cache.contains(uri) && callback != null) {
             new Handler(Looper.getMainLooper()).postAtFrontOfQueue(new SuccessRunnable(cache.get(uri)));
             return;
         }
 
+        //Dont cache in network, because we will have it downloaded.
         CacheControl cacheControl = new CacheControl.Builder()
                 .noCache()
                 .noStore()
@@ -103,6 +123,10 @@ final class Streamer {
         });
     }
 
+    /**
+     * Method for successfully downloading the image
+     * @param response from network
+     */
     private void onResponse(@NonNull Response response) {
         try {
             File file = cache.put(uri, response.body().byteStream());
@@ -112,6 +136,9 @@ final class Streamer {
         }
     }
 
+    /**
+     * Package access inner builder class for creating a Streamer
+     */
     static class Builder {
 
         private OkHttpClient client = null;
@@ -119,23 +146,46 @@ final class Streamer {
 
         private Cache cache = null;
 
+        /**
+         * Empty constructor
+         */
         Builder() {}
 
+        /**
+         * Set a custom networking client
+         * @param client for network calls
+         * @return Builder
+         */
         public Builder client(@NonNull OkHttpClient client) {
             this.client = client;
             return this;
         }
 
+        /**
+         * Set custom callbacks for the status of the request
+         * @param callback for receiving status updates
+         * @return Builder
+         */
         public Builder callback(@NonNull FileCallback callback) {
             this.callback = callback;
             return this;
         }
 
+        /**
+         * Set custom cache for managing the images in disk
+         * @param cache used for managing the images in disk
+         * @return Builder
+         */
         public Builder cache(@NonNull Cache cache) {
             this.cache = cache;
             return this;
         }
 
+        /**
+         * Create a streamer and fetch the given uri
+         * @param uri to fetch from network or cache if already exists
+         * @return Streamer instance for the given params
+         */
         public Streamer fetch(@NonNull Uri uri) {
             Validator.checkNullAndThrow(this, uri);
 
@@ -149,6 +199,9 @@ final class Streamer {
 
     }
 
+    /**
+     * Inner class for a Failure networking event
+     */
     class FailureRunnable implements Runnable {
         private @NonNull Exception exception;
 
@@ -164,6 +217,9 @@ final class Streamer {
         }
     }
 
+    /**
+     * Inner class for a Success networking event
+     */
     class SuccessRunnable implements Runnable {
         private @Nullable File file;
 
